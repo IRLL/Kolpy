9bd4a2fd5ead7a6b34276ebdcb3bb42c75018beb
Mathïs Fédérico <mathfederico@gmail.com>
2022-12-01 21:48:51 +0100

    :tada: Add argsparse CLI

--- src/kolpy/__main__.py
+++ src/kolpy/__main__.py
@@ -6,11 +6,22 @@ python -m kolpy
 """
 
 import kolpy
+import argparse
+
+
+def build_parser() -> argparse.ArgumentParser:
+    """Build the CLI parser of the Kolpy package."""
+    parser = argparse.ArgumentParser(
+        description="Compute upper bounds of kolmogorov complexity of a python code",
+    )
+    return parser
 
 
 def main() -> None:
     """Entry point of the package"""
-    print(f"Hello world from {kolpy.__name__} ({kolpy.__doc__})")
+    parser = build_parser()
+    print(f"Hello world from {parser.prog.capitalize()} ({parser.description})")
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":

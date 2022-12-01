105c6640fde78cabc9f5c69dbc86ba80b3faa18c
Mathïs Fédérico <mathfederico@gmail.com>
2022-12-01 21:32:53 +0100

    :tada: Rename package to kolpy

--- src/kolpy/__main__.py
+++ src/kolpy/__main__.py
@@ -1,16 +1,16 @@
 """Entry module of the package.
 
 You can run this script using:
-python -m mypackage
+python -m kolpy
 
 """
 
-import mypackage
+import kolpy
 
 
 def main() -> None:
     """Entry point of the package"""
-    print(f"Hello world from {mypackage.__name__} ({mypackage.__doc__})")
+    print(f"Hello world from {kolpy.__name__} ({kolpy.__doc__})")
 
 
 if __name__ == "__main__":
--- tests/mypackage/unit/test_mymodule.py
+++ tests/mypackage/unit/test_mymodule.py
@@ -1,6 +1,6 @@
 import pytest_check as check
 
-from mypackage.mymodule import my_perfect_function
+from kolpy.mymodule import my_perfect_function
 
 
 def test_fizz():
